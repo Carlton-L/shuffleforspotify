add726ce0f132ef613169b542f08098ff84f3c8d
Carlton Lindsay <carlton.l@outlook.com>
2021-10-18 14:24:34 -0700

    Fixed some broken fonts

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -6,26 +6,32 @@ import Seo from '../components/seo.js';
 import Header from '../components/header.js'
 import Footer from '../components/footer.js'
 import Theme from '../themes/theme.js';
-import GothamSSmMedium from '../fonts/GothamSSm-Medium.woff2';
-import GothamSSmBold from '../fonts/GothamSSm-Bold.woff2';
-import GothamSSmBook from '../fonts/GothamSSm-Book.woff2';
+import GothamSSmMediumttf from '../fonts/GothamSSm-Medium.woff2';
+import GothamSSmMediumwoff from '../fonts/GothamSSm-Medium.woff2';
+import GothamSSmBoldttf from '../fonts/GothamSSm-Bold.woff2';
+import GothamSSmBoldwoff from '../fonts/GothamSSm-Bold.woff2';
+import GothamSSmBookttf from '../fonts/GothamSSm-Book.woff2';
+import GothamSSmBookwoff from '../fonts/GothamSSm-Book.woff2';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
   @font-face {
     font-family: "GothamSSm-Bold";
     src: local('GothamSSm-Bold'),
-    url(${GothamSSmBold}) format('woff2')
+    url(${GothamSSmBoldwoff}) format('woff2'),
+    url(${GothamSSmBoldttf}) format('ttf');
   }
   @font-face {
     font-family: "GothamSSm-Book";
     src: local('GothamSSm-Book'),
-    url(${GothamSSmBook}) format('woff2');
+    url(${GothamSSmBookwoff}) format('woff2'),
+    url(${GothamSSmBookttf}) format('ttf');
   }
   @font-face {
     font-family: "GothamSSm-Medium";
     src: local('GothamSSm-Medium'),
-    url(${GothamSSmMedium}) format('woff2');
+    url(${GothamSSmMediumwoff}) format('woff2'),
+    url(${GothamSSmMediumttf}) format('ttf');
   }
   body {
     box-sizing: border-box;
