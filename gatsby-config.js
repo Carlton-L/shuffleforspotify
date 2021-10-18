a834be5b6d911ed7468e776fb4af0f8d1d462090
Carlton Lindsay <carlton.l@outlook.com>
2021-10-18 14:43:27 -0700

    Update gatsby-config.js
    Changed og image from png to jpeg

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     siteUrl: 'https://www.fakedomain.tld',
     title: 'Shuffle for Spotify',
     description: 'True randomized shuffle WITHOUT Spotify\'s weighted algorithm.',
-    image: 'https://res.cloudinary.com/carlton-dev/image/upload/v1634590873/Shuffle-for-Spotify/ShuffleForSpotify-og-01_xm7xyn.png'
+    image: 'https://res.cloudinary.com/carlton-dev/image/upload/v1634593368/Shuffle-for-Spotify/ShuffleForSpotify-og-01_rpa89w.jpg'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
