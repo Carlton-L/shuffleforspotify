0e4cacb271ac1eec5b651661db2764e0d5fdd0cb
Carlton Lindsay <carlton.l@outlook.com>
2021-10-18 20:22:08 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,16 @@ module.exports = {
     siteUrl: 'https://www.fakedomain.tld',
     title: 'Shuffle for Spotify',
     description: 'True randomized shuffle WITHOUT Spotify\'s weighted algorithm.',
-    image: 'https://res.cloudinary.com/carlton-dev/image/upload/v1634593368/Shuffle-for-Spotify/ShuffleForSpotify-og-01_rpa89w.jpg'
+    image: 'https://res.cloudinary.com/carlton-dev/image/upload/v1634593368/Shuffle-for-Spotify/ShuffleForSpotify-og-01_rpa89w.jpg',
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-BG0K10FJH3", // Google Analytics / GA
+          'G-BG0K10FJH3', // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
@@ -26,17 +27,16 @@ module.exports = {
           // Setting this parameter is also optional
           respectDNT: false,
           // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
         },
       },
     },
-    'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
-  pathPrefix: "/shuffleforspotify",
-}
+  pathPrefix: '/shuffleforspotify',
+};
 
 // Measurement ID G-BG0K10FJH3
