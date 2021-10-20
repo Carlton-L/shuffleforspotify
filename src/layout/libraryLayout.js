42368a0ac1552a3f926665765fd349a190924c96
Carlton Lindsay <carlton.l@outlook.com>
2021-10-20 15:22:26 -0700

    Create libraryLayout.js

---
+++ src/layout/libraryLayout.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Seo from '../components/seo.js';
+import Theme from '../themes/theme.js';
+import GlobalStyle from './globalStyle';
+
+const LibraryLayout = ({
+  children,
+  title = false,
+  description = false,
+  image = false,
+  path = false,
+  props,
+}) => {
+
+}
+
+export default LibraryLayout;
