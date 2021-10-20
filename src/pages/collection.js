4653e8111ffc1298ca12a259b6cafa380957c03c
Carlton Lindsay <carlton.l@outlook.com>
2021-10-20 15:22:34 -0700

    Create collection.js

---
+++ src/pages/collection.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import LibraryLayout from '../layout/libraryLayout.js';
+
+const CollectionPage = () => {
+  return (
+    'henlo'
+  )
+}
+
+export default CollectionPage;
