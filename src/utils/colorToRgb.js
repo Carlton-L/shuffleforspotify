7620f32986a2569fd2de33633b19055414b8673a
Carlton Lindsay <carlton.l@outlook.com>
2021-10-26 13:47:29 -0700

    Update colorToRgb.js

---
+++ src/utils/colorToRgb.js
@@ -0,0 +1,295 @@
+
+// Accepts a Web Color String or Hex string (#xxxxxx) and outputs RGB values
+const colorToRgb = (input) => {
+  switch(color) {
+    // Pink
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'DeepPink':
+      return {red: 255, green: 20, blue: 147}
+    case 'PaleVioletRed':
+      return {red: 219, green: 112, blue: 147}
+    case 'HotPink':
+      return {red: 255, green: 105, blue: 180}
+    case 'LightPink':
+      return {red: 255, green: 182, blue: 193}
+    case 'Pink':
+      return {red: 255, green: 192, blue: 203}
+
+    // Red
+    case 'DarkRed':
+      return {red: 139, green: 0, blue: 0}
+    case 'Red':
+      return {red: 255, green: 0, blue: 0}
+    case 'FireBrick':
+      return {red: 178, green: 34, blue: 34}
+    case 'Crimson':
+      return {red: 220, green: 20, blue: 60}
+    case 'IndianRed':
+      return {red: 205, green: 92, blue: 92}
+    case 'LightCoral':
+      return {red: 240, green: 128, blue: 128}
+    case 'Salmon':
+      return {red: 250, green: 128, blue: 114}
+    case 'DarkSalmon':
+      return {red: 233, green: 150, blue: 122}
+    case 'LightSalmon':
+      return {red: 255, green: 160, blue: 122}
+    case 'OrangeRed':
+      return {red: 255, green: 69, blue: 0}
+    case 'Tomato':
+      return {red: 255, green: 99, blue: 71}
+    case 'DarkOrange':
+      return {red: 255, green: 140, blue: 0}
+    case 'Coral':
+      return {red: 255, green: 127, blue: 80}
+    case 'Orange':
+      return {red: 255, green: 165, blue: 0}
+    
+    // Yellow
+    case 'DarkKhaki':
+      return {red: 189, green: 183, blue: 107}
+    case 'Gold':
+      return {red: 255, green: 215, blue: 0}
+    case 'Khaki':
+      return {red: 240, green: 230, blue: 140}
+      case 'PeachPuff':
+      return {red: 255 , green: 218, blue: 185}
+    case 'Yellow':
+      return {red: 255, green: 255, blue: 0}
+    case 'PaleGoldenrod':
+      return {red: 238, green: 232, blue: 170}
+    case 'Moccasin':
+      return {red: 255, green: 228, blue: 181}
+    case 'PapayaWhip':
+      return {red: 255, green: 239, blue: 213}
+    case 'LightGoldenrodYellow':
+      return {red: 250, green: 250, blue: 210}
+    case 'LemonChiffron':
+      return {red: 255, green: 250, blue: 205}
+    case 'LightYellow':
+      return {red: 255, green: 255, blue: 224}
+
+    // Brown
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+    case 'MediumVioletRed':
+      return {red: 199, green: 21, blue: 133}
+  }
+}
