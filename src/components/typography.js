17bedc246c4952f8f803af83ef88b7d3c058114d
Carlton Lindsay <carlton.l@outlook.com>
2021-10-27 13:34:54 -0700

    Update typography.js

--- src/components/typography.js
+++ src/components/typography.js
@@ -6,5 +6,9 @@ Typography props/options
 align: string (center) - sets the text-align property
 variant: string (body) - sets the variant
 
-
+Element type is always <span>
 */
+
+const Typography = () => {
+  
+}
