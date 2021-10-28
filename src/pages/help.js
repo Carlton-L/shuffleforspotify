b04fe03b4cd3311368eb751fe56d78f51c7a45a3
Carlton Lindsay <carlton.l@outlook.com>
2021-10-28 13:53:42 -0700

    Create help.js

---
+++ src/pages/help.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import MainLayout from '../layout/mainLayout.js';
+
+/*
+Help page includes the following information:
+
+* How to log out so another user can log in
+* ...
+
+*/
+
+const HelpPage = () => {
+  return (
+    'henlo'
+  )
+}
+
+export default HelpPage;
