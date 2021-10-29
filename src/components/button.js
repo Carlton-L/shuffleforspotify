03afa36102ad306072aded12ce23d661a0af9759
Carlton Lindsay <carlton.l@outlook.com>
2021-10-29 14:00:21 -0700

    Update button.js

--- src/components/button.js
+++ src/components/button.js
@@ -3,15 +3,101 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const StyledButton = styled.button`
-  background-color: ${props => {
-  }}
+  font-size: 14px;
+  text-transform: uppercase;
+  letter-spacing: 0.125rem;
+  padding: 12px 32px;
+  border-radius: 999px;
+  border-width: 2px;
+  border-style: solid;
+  border-color: ${props => props.variant === 'outline' ? props.color.main : 'transparent'};
+  font-color: ${props => props.variant === 'outline' ? props.color.main : (
+    props.variant === 'disabled' ? 'darkgrey' : props.color.contrastText
+  )};
+  background-color: ${props => props.variant === 'filled' ? props.color.main : (
+    props.variant === 'disabled' ? 'grey' : 'transparent'
+  )};
+
+  &:active {
+    border-color: ${props => props.variant === 'outline' ? props.color.dark : 'transparent'};
+    background-color: ${props => props.variant === 'filled' ? props.color.dark : (
+      props.variant === 'disabled' ? 'grey' : 'transparent'
+    )};
+    font-color: ${props => props.variant === 'filled' ? props.color.dark : (
+      props.variant === 'disabled' ? 'grey' : 'transparent'
+    )};
+  }
+
+  &:hover {
+    border-color: ${props => props.variant === 'outline' ? props.color.light : 'transparent'};
+    background-color: ${props => props.variant === 'filled' ? props.color.dark : (
+      props.variant === 'disabled' ? 'grey' : 'transparent'
+    )};
+    font-color: ${props => props.variant === 'filled' ? props.color.dark : (
+      props.variant === 'disabled' ? 'grey' : 'transparent'
+    )};
+  }
 `
 
 const Button = ({children, color, variant}) => {
-
+  return (
+    <StyledButton color={color} variant={variant}>{children}</StyledButton>
+  )
 }
 
 Button.propTypes = {
   color: PropTypes.oneOf(['primary', 'secondary', 'white', 'black']),
   variant: PropTypes.oneOf(['text', 'filled', 'outline', 'disabled'])
 }
+
+Button.defaultProps = {
+  color: 'primary',
+  variant: 'filled'
+}
+
+export default Button;
+
+/* 
+Variant:
+text: no border, no fill, color text
+filled: no border, color fill, contrast text
+outline: color border, no fill, color text
+disabled: no border, grey fill, dark grey text
+
+Color:
+main color, light hover, dark active
+
+Attributes:
+
+border-radius: 50%
+border-width: 2px
+border-style: solid;
+border-color: props.variant === outline ? border : transparent
+font-color: props.variant === outline ? props.color.main : (
+  props.variant === disabled ? darkgrey : props.color.contrastText
+)
+background-color: props.variant === filled ? props.color.main : (
+  props.variant === disabled ? grey : transparent
+)
+
+&:active {
+  border-color: props.variant === outline ? props.color.dark : transparent
+  background-color: props.variant === filled ? props.color.dark : (
+    props.variant === disabled ? grey : transparent
+  )
+  font-color: props.variant === filled ? props.color.dark : (
+    props.variant === disabled ? grey : transparent
+  )
+}
+
+&:hover {
+  border-color: props.variant === outline ? props.color.light : transparent
+  background-color: props.variant === filled ? props.color.dark : (
+    props.variant === disabled ? grey : transparent
+  )
+  font-color: props.variant === filled ? props.color.dark : (
+    props.variant === disabled ? grey : transparent
+  )
+}
+
+*/
