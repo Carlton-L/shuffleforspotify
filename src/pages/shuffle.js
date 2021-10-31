be7762c93caaaef250344dcbdd857c5d0d3d9e9e
Carlton Lindsay <carlton.l@outlook.com>
2021-10-30 20:40:14 -0700

    Create shuffle.js

---
+++ src/pages/shuffle.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import LibraryLayout from '../layout/libraryLayout.js';
+import ContentContainer from '../components/contentContainer.js';
+import LoadingSpinner from '../components/loadingSpinner';
+import Paper from '../components/paper';
+import useLocalState from '../hooks/useLocalState.js';
+import ErrorDialog from '../components/errorDialog'
+import Button from '../components/button.js';
+
+const ShufflePage = ({ location }) => {
+  return (
+    <LibraryLayout>
+      <ContentContainer>
+        
+      </ContentContainer>
+    </LibraryLayout>
+  )
+}
+
+export default ShufflePage;
