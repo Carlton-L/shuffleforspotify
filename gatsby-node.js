706f4c9d97f97680af0dce1fc2af90accf172f7c
Carlton Lindsay <carlton.l@outlook.com>
2021-11-03 13:22:52 -0700

    Create gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,9 @@
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  if (getConfig().mode === 'production') {
+    actions.setWebpackConfig({
+      devtool: false
+    });
+  }
+};
+
+// Disable sourcemaps in production
