176ae903526a144b5b687acd7b66e5d9a0ca68c3
Carlton Lindsay <carlton.l@outlook.com>
2021-11-03 15:07:52 -0700

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import MainLayout from '../layout/mainLayout.js';
+import FourOhFour from '../components/404.js'
+
+const IndexPage = () => {
+
+  return (
+    <MainLayout>
+      <FourOhFour />
+    </MainLayout>
+  )
+}
+
+export default IndexPage;
