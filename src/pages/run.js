94411f599e447d4a1e66ec16a359107e42fb930e
Carlton Lindsay <carlton.l@outlook.com>
2021-11-16 16:37:46 -0800

    Update run.js

--- src/pages/run.js
+++ src/pages/run.js
@@ -12,39 +12,13 @@ import PlaylistCard from '../components/playlistCard.js';
 // Playlist ID: location.state.list.id
 // Library
 
-const fetchSongs = () => {
-
-};
-
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
-
-const shuffleArray = (inputArray) => {
-
-};
-
-const findNonMovers = (shuffledArray) => {
-  // {First Element, Last Element, Compare Element, Elements[], Array[]}
-  shuffledArray.forEach((element, index, array) => {
-    // Starting element = element
-    const startingElement = element;
-    array.forEach((element, index, array) => {
-
-    });
-  });
-  // { Starting Element, Compare Element, Elements[] }
-  //  Select the first element, set it to Starting Element and Compare Element
-  // Add its index to Elements[]
-  // Iterate through the shuffledArray, comparing each element to Compare Element
-  // If any element is higher tham the Compare Element, add its index to Elements[]
-  // And set Compare Element to its value { value, index } and continue
-  // When the end of the list is reached, store Elements[]
-  // Then re-start the commparison, with the same StartingElement, but comparing only elements
-  // After the first element in Elements[]
-  // Store the new Elements[]
-  // Then repeat the process again, starting at the first element in the new Elements[]
-  // Stop when there are no more elements in the remaining list (between the new first element
-  // in elements and the end of the array) that are greater then the starting element
-  // Start the entire process over again, selecting a new Starting Element (Starting element index +1)
+// Create an array of length n where each element is equal to its index
+const createArray = (n) => {
+  const array = [];
+  for (let i = 0; i < n; i++) {
+    array.push(i);
+  }
+  return array;
 };
 
 const RunPage = ({ location }) => {
